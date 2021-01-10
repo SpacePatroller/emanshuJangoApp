fd37aa89cf9bdfc3140535167872cd665f763868
SpacePatroller <emanshupatel@Emanshus-MacBook-Pro.local>
2021-01-10 11:12:28 -0600

    base

--- hello/views.py
+++ hello/views.py
@@ -4,9 +4,11 @@ from django.http import HttpResponse
 from .models import Greeting
 
 # Create your views here.
+
+
 def index(request):
     # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+    return render(request, "base.html")
 
 
 def db(request):
