dee37696e6a707c41c06839fa1deb8eb29929c13
SpacePatroller <emanshupatel@Emanshus-MacBook-Pro.local>
2021-01-13 21:35:47 -0600

    working

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = "gettingstarted.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE" : "django.db.backends.sqlite3",
+        "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3")
     }
 }
@@ -115,5 +115,8 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'img/')
+MEDIA_URL = '/img/'
 
 django_heroku.settings(locals())
